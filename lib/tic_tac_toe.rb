edbfd225800138ebb84b3498a72d9e4a42568109
Devan Moylan <dmmoylan@gmail.com>
2017-04-24 20:15:38 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,121 @@
+class TicTacToe
+
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2],
+  ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(input)
+    @index = input.to_i - 1
+  end
+
+  def move(index, token = "X")
+    @board[index] = token
+  end
+
+  def position_taken?(index)
+    @board[index] == "X" || @board[index] == "O"
+  end
+
+  def valid_move?(index)
+    index.between?(0, 8) && !position_taken?(index)
+  end
+
+  def turn
+    puts "Where would you like to go 1-9?"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
+  def won?
+    WIN_COMBINATIONS.find do |win_combination|
+        win_index_1 = win_combination[0]
+        win_index_2 = win_combination[1]
+        win_index_3 = win_combination[2]
+
+        position_1 = @board[win_index_1] # load the value of the board at win_index_1
+        position_2 = @board[win_index_2] # load the value of the board at win_index_2
+        position_3 = @board[win_index_3] # load the value of the board at win_index_3
+
+        if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+          return win_combination # return the win_combination indexes that won.
+        else
+          false
+        end
+    end
+  end
+
+  def full?
+    turn_count == 9
+  end
+
+  def draw?
+    turn_count == 9 && !won?
+  end
+
+  def over?
+    draw? || won? || full?
+  end
+
+  def winner
+    WIN_COMBINATIONS.find do |win_combination|
+        win_index_1 = win_combination[0]
+        win_index_2 = win_combination[1]
+        win_index_3 = win_combination[2]
+
+        position_1 = @board[win_index_1] # load the value of the board at win_index_1
+        position_2 = @board[win_index_2] # load the value of the board at win_index_2
+        position_3 = @board[win_index_3] # load the value of the board at win_index_3
+
+        if (position_1 == "X" && position_2 == "X" && position_3 == "X")
+          return "X"
+        elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
+          return "O"
+        end
+    end
+  end
+
+  def play
+    until over? do
+      turn
+    end
+    if won?
+      puts "Congratulations " + winner + "!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
+
+end
